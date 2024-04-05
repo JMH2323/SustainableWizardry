a995d59f191d51ba2efb337b2c6e08b673b4f4da
JMH2323 <johnmasonhaley23@gmail.com>
2024-04-04 23:25:50 -0400

    Damage Execution Calculation Core
    Takes in attribute definitions and applies complex math for damage. Currently uses armor, TODO: dodge chance, scaling, and other RPG-like concepts

--- Source/SustainableWizardry/GAS/ExecCalc/ExecCalc_Damage.cpp
+++ Source/SustainableWizardry/GAS/ExecCalc/ExecCalc_Damage.cpp
@@ -5,17 +5,21 @@
 
 #include "SustainableWizardry/GAS/Attribute/SusWizAttributeSet.h"
 #include "AbilitySystemComponent.h"
+#include "VectorVM.h"
+#include "SustainableWizardry/SusWizGameplayTags.h"
 
 struct SusWizDamageStatics
 {
 
 	// Creates the capture def to be defined below
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPen);
 	
 	SusWizDamageStatics()
 	{
 		// Defined the armor capture definition. Set, def, source, and boolean(snapshot)
 		DEFINE_ATTRIBUTE_CAPTUREDEF(USusWizAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(USusWizAttributeSet, ArmorPen, Source, false);
 	}
 };
 
@@ -30,6 +34,7 @@ UExecCalc_Damage::UExecCalc_Damage()
 {
 	// Add the definitions to the attribute capture. lets us finds those attributes we want from the definitions.
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorPenDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -56,19 +61,54 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	EvaluateParameters.SourceTags = SourceTags;
 	EvaluateParameters.TargetTags = TargetTags;
 
-	// Local Attributes initialized for population and use
-	float Armor = 0.f;
+
+	// Get Damage set by the caller magnitude
+	float Damage = Spec.GetSetByCallerMagnitude(FSusWizGameplayTags::Get().Damage);
 	
-	// Get those attributes from the capture list and clamp them if needed
-	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, Armor);
-	Armor = FMath::Max<float>(0.f, Armor);
+
+	
 
 
-	// DO MATH
+	/*
+	 * DO MATH
+	 * Scale first
+	 * Armor second
+	 * Dodge last
+	 */
+
+	/* TODO: Scale Damage?? */
+
+	
+	/* Armor */
+	// Capture armor and armor penetration. Reduce damage by percentage
+	float TargetArmor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, TargetArmor);
+	TargetArmor = FMath::Max<float>(TargetArmor, 0.f);
+
+	float SourceArmorPen = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorPenDef, EvaluateParameters, SourceArmorPen);
+	SourceArmorPen = FMath::Max<float>(SourceArmorPen, 0.f);
+
+	float ArmorApplied = TargetArmor - SourceArmorPen;
+	
+	Damage *= (1.0f - (ArmorApplied / 100.f));
+
+	
+	/* TODO: Dodge Chance */
+	// Capture dodge change, if dodged, negate all damage.
+	float TargetDodgeChance = 0.f;
+	
+	// Use this bool for dodging
+	const bool bDodged = FMath::RandRange(1, 100) < TargetDodgeChance;
+	Damage = bDodged ? Damage = 0.f : Damage;
 
 
+	/*
+	 * Math done
+	 */
+	
 	// Output as Evaluated Data
-	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	const FGameplayModifierEvaluatedData EvaluatedData(USusWizAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 	
 	
